0e3d3e5fc5216d9864bc76d2a52b0b22a7e036d9
Georgi-Zahariev <zahariev.georgi03@gmail.com>
2021-09-26 14:17:14 +0300

    School project after corrections.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Scanner;
    
@@ -15,7 +17,7 @@ class cmp implements Comparator<TeamBase> {
 	public int compare(TeamBase a, TeamBase b) {
 		if (a.avHeight() < b.avHeight())
 			return 1;
-		if (a.avHeight() < b.avHeight())
+		if (a.avHeight() > b.avHeight())
 			return -1;
 		return 0;
 	}
@@ -42,18 +44,16 @@ abstract class TeamBase implements VolleyballTeam, BasketballTeam {
 	}
 
 	public double avHeight() {
-		int a = height.length;
 		double sum = 0;
-		for (int i = 0; i < a; i++) {
+		for (int i = 0; i < playerCount; i++) {
 			sum += height[i];
 		}
-		return sum / a;
+		return sum / playerCount;
 	}
 
 	public double maxHeight() {
-		int a = height.length;
-		double max = 0;
-		for (int i = 0; i < a; i++) {
+		double max = height[0];
+		for (int i = 1; i < playerCount; i++) {
 			if (height[i] > max)
 				max = height[i];
 		}
@@ -66,12 +66,12 @@ abstract class TeamBase implements VolleyballTeam, BasketballTeam {
 
 	@Override
 	public String getBasketballTeamName() {
-	     return "Voleyball Team: \"" + name + " V.C.\"";
+	     return "Basketball Team: \"" + name + " B.C.\"";
 	}
 
 	@Override
 	public String getVolleyballTeamName() {
-		return "Basketball Team: \"" + name + " V.C.\"";
+		return "Volleyball Team: \"" + name + " V.C.\"";
 	}
 }
 	class Team extends TeamBase {
@@ -90,14 +90,16 @@ abstract class TeamBase implements VolleyballTeam, BasketballTeam {
 		public Team getData(String lineRead) {
 			String[] a = lineRead.split(",");
 
-			int br = Integer.parseInt(a[2]);
-			double[] h = new double[br];
-			for (int i = 0; i < br; i++) {
-				h[i] = Double.parseDouble(a[i + 3]);
-			}
 			String kk = a[0];
 			char k = kk.charAt(0);
 			String n = a[1];
+			
+			int br = a.length-3;
+			double[] h = new double[br];
+			for (int i = 0; i < br; i++) {
+				h[i]= Double.parseDouble(a[i + 3]);
+			}
+			
 			return new Team(k, n, br, h);
 		}
 
@@ -105,7 +107,7 @@ abstract class TeamBase implements VolleyballTeam, BasketballTeam {
 
 		public String toString() {
 			return (kind == 'V' ? getVolleyballTeamName() : getBasketballTeamName()) +
-					 name + "\" , Players: " + getPlayerCount() + ", average height: "
+					 ", Players: " + getPlayerCount() + ", average height: "
 					+ String.format("%.2f", avHeight()) + ", maximal height: " + String.format("%.2f", maxHeight());
 		}
 
@@ -113,26 +115,38 @@ abstract class TeamBase implements VolleyballTeam, BasketballTeam {
 
 
 
-public class Main {
-
-	public static void main(String[] args) throws Exception {
-		ArrayList<Team> teams = new ArrayList<Team>();// Динамичен масив от отбори
-		File file = new File("data.txt");// Отваряне на файла
-		Scanner sc = new Scanner(file);// Скенер за четене от файл
-		while (sc.hasNextLine()) {// Докато има редове
-			Team t = new Team(); // Създаване "празен" отбор
-			String s = sc.nextLine(); // Четем следващ ред
-			if (!s.equals("")) { // Ако не е празен ред
-				t = t.getData(sc.nextLine());// Пренасочваме t към нов обект от тип "Отбор"
-				teams.add(t);// Добавяме към динамичния масив от отбори
-			}
+	public class Main {
+		 public static void main(String[] args) throws Exception {
+		  ArrayList<Team> teams=new ArrayList<Team>();//Динамичен масив от отбори
+		  File file = new File("data.txt");//Отваряне на файла
+		  Scanner sc = new Scanner(file);//Скенер за четене от файл
+		  while (sc.hasNextLine()) {//Докато има редове
+		   Team t=new Team(); //Създаване "празен" отбор
+		    t=t.getData(sc.nextLine());//Пренасочваме t към нов обект от тип "Отбор"
+		    teams.add(t);//Добавяме към динамичния масив от отбори  
+		  }
+		  sc.close();
+		  teams.sort(new cmp());//Подреждаме отборите по указания приоритет
+		  //Извеждаме подредения масив на стандартния изход
+		  for (Team t:teams) System.out.println(t);
+			 
+			/* ArrayList<Team> teams=new ArrayList<Team>();
+			  File file = new File("data.txt");
+			  try {
+			 Scanner inp= new Scanner(file);
+			 while(inp.hasNext()) {
+				 Team a= new Team();
+				 a=a.getData(inp.nextLine());
+				 teams.add(a);
+			 }
+			 inp.close();
+			  }catch( FileNotFoundException e) {
+				  System.out.println("File not found!");
+			  }
+			  //teams.sort(new cmp());
+			  cmp comp = new cmp();
+			  Collections.sort(teams,comp);
+			  for (Team t:teams) System.out.println(t.toString());
+			  */
+		 }
 		}
-		sc.close();
-		teams.sort(new cmp());// Подреждаме отборите по указания приоритет
-		// Извеждаме подредения масив на стандартния изход
-		for (Team t : teams)
-			System.out.println(t);
-
-	}
-
-}
